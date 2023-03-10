fae0592a0fba0635cb3699d402ad4debd7e69f54
bnparham <parhambn.workmail@gmail.com>
2023-03-10 21:20:59 +0330

    dynamic time reservation with using state in application - remove reserved time from list

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -1,9 +1,24 @@
 import BookingForm from '../components/Reservation/BookingForm';
+import { useState } from 'react';
 
 const BookingPage = () => {
+    const [availableTimes ,setAvailableTimes] = useState(
+        [{id:0,time:"17:00"},
+        {id:1,time:"18:00"},
+        {id:2,time:"19:00"},
+        {id:3,time:"20:00"},
+        {id:4,time:"21:00"},
+        {id:5,time:"22:00"},]
+    )
+
+    const updateTimes = (id) => {
+        const newTime = availableTimes.filter((item) => item.id !== parseInt(id))
+        setAvailableTimes(newTime)
+    }
+
     return(
         <>
-            <BookingForm/>
+            <BookingForm times={availableTimes} updateTimes={updateTimes}/>
         </>
     )
 }
--- src/components/Reservation/BookingForm.jsx
+++ src/components/Reservation/BookingForm.jsx
@@ -1,39 +1,69 @@
+import { useState,useRef } from "react";
 import reservePhoto from "../../Assets/icons_assets/Mario and Adrian A.jpg"
-const BookinghtmlForm = () => {
+
+
+const BookinghtmlForm = (props) => {
+
+    const [resdate, setResdate] = useState("");
+    const [restime, setRestime] = useState("17:00");
+    const [guests, setGuests] = useState("1");
+    const [occasion, setOccasion] = useState("Birthday");
+
+    const timeId = useRef(0)
+
+    const resDateHandeler = (e) => setResdate(e.target.value)
+    const resTimeHandeler = (e) => {
+        setRestime(e.target.value)
+        timeId.current = e.target.options[e.target.selectedIndex].id
+    }
+    const guestHandeler = (e) => setGuests(e.target.value)
+    const occasionHandeler = (e) => setOccasion(e.target.value)
+
+    const submitHandeler = (e) => {
+        e.preventDefault();
+        console.log(resdate);
+        console.log(restime);
+        console.log(guests);
+        console.log(occasion);
+        props.updateTimes(timeId.current);
+    }
+
+
     return(
         <div className="container-md">
             <div className="row">
                 <div id="reservation-form" className="col-md-6">
                     <h4 id="reserve-title">Reservation Form</h4>
-                    <form className="row">
+                    <form onSubmit={submitHandeler} className="row">
                         <div className="col-md-6">
                             <label class="form-label" htmlFor="res-date">Choose date</label>
-                            <input className="form-control" type="date" id="res-date"/>
+                            <input className="form-control" type="date" id="res-date" value={resdate} onChange={resDateHandeler}/>
                         </div>
                         <div className="col-md-6">
                             <label  class="form-label" htmlFor="res-time">Choose time</label>
-                            <select className="form-select" id="res-time ">
-                                <option>17:00</option>
-                                <option>18:00</option>
-                                <option>19:00</option>
-                                <option>20:00</option>
-                                <option>21:00</option>
-                                <option>22:00</option>
+                            <select value={restime} onChange={resTimeHandeler} className="form-select" id="res-time">
+                            {props.times.map(
+                                (obj)=>{
+                                    return(
+                                    <option value={obj.time} id={obj.id} key={obj.id}>{obj.time}</option>
+                                    )
+                                }
+                            )}
                             </select>
                         </div>
                         <div className="col-md-12">
                             <label class="form-label" htmlFor="guests">Number of guests</label>
-                            <input className="form-control" type="number" placeholder="1" min="1" max="10" id="guests"/>
+                            <input value={guests} onChange={guestHandeler} className="form-control" type="number" placeholder="1" min="1" max="10" id="guests"/>
                         </div>
                         <div className="col-md-12">
                             <label class="form-label" htmlFor="occasion">Occasion</label>
-                            <select className="form-select" id="occasion">
-                                <option>Birthday</option>
-                                <option>Anniversary</option>
+                            <select value={occasion} onChange={occasionHandeler} className="form-select" id="occasion">
+                                <option value="Birthday">Birthday</option>
+                                <option value="Anniversary">Anniversary</option>
                             </select>
                         </div>
                         <div id="submit-reserve" className="col-md-12">
-                            <input  className="btn btn-outline-success w-50" type="submit" value="Make Your reservation"/>
+                            <input className="btn btn-outline-success w-50" type="submit" value="Make Your reservation"/>
                         </div>
                     </form>
                 </div>
