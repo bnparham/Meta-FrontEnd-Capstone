f37b3b823c0a956b44689202e62202c4197bd561
bnparham <parhambn.workmail@gmail.com>
2023-03-16 13:16:47 +0330

    refactor BookingForm component using Formik and Yup + add Validations

--- src/components/Reservation/BookingForm.jsx
+++ src/components/Reservation/BookingForm.jsx
@@ -1,79 +1,119 @@
-import { useState,useRef } from "react";
+import {useRef } from "react";
 import reservePhoto from "../../Assets/icons_assets/Mario and Adrian A.jpg"
-
+import {Formik,Form,Field,ErrorMessage} from 'formik'
+import { formSchema } from './../Validation/BookingFormValidation';
 
 const BookinghtmlForm = (props) => {
 
-    const [resdate, setResdate] = useState(new Date().toDateString());
-    const [restime, setRestime] = useState("17:00");
-    const [guests, setGuests] = useState("1");
-    const [occasion, setOccasion] = useState("Birthday");
-
     const timeId = useRef(0)
 
-    const resDateHandeler = (e) => setResdate(e.target.value)
     const resTimeHandeler = (e) => {
-        setRestime(e.target.value)
         timeId.current = e.target.options[e.target.selectedIndex].id
     }
-    const guestHandeler = (e) => setGuests(e.target.value)
-    const occasionHandeler = (e) => setOccasion(e.target.value)
-
-    const submitHandeler = (e) => {
-        e.preventDefault();
-        props.updateTimes(timeId.current);
-        props.submitForm(
-            {
-                resDate : resdate,
-                restime : restime,
-                guests : guests,
-                occasion : occasion
-            }
-        )   
-        // console.log(resdate);
-        // console.log(restime);
-        // console.log(guests);
-        // console.log(occasion);
-    }
-
 
     return(
         <div className="container-md">
             <div className="row">
                 <div id="reservation-form" className="col-md-6">
                     <h4 id="reserve-title">Reservation Form</h4>
-                    <form onSubmit={submitHandeler} className="row">
-                        <div className="col-md-6">
-                            <label class="form-label" htmlFor="res-date">Choose date</label>
-                            <input className="form-control" type="date" id="res-date" value={resdate} onChange={resDateHandeler}/>
-                        </div>
-                        <div className="col-md-6">
-                            <label  class="form-label" htmlFor="res-time">Choose time</label>
-                            <select value={restime} onChange={resTimeHandeler} className="form-select" id="res-time">
-                            {props.times.map(
-                                (obj)=>{
-                                    return(
-                                    <option value={obj.time} id={obj.id} key={obj.id}>{obj.time}</option>
-                                    )
-                                }
-                            )}
-                            </select>
-                        </div>
-                        <div className="col-md-12">
-                            <label class="form-label" htmlFor="guests">Number of guests</label>
-                            <input value={guests} onChange={guestHandeler} className="form-control" type="number" placeholder="1" min="1" max="10" id="guests"/>
-                        </div>
-                        <div className="col-md-12">
-                            <label class="form-label" htmlFor="occasion">Occasion</label>
-                            <select value={occasion} onChange={occasionHandeler} className="form-select" id="occasion">
-                                <option value="Birthday">Birthday</option>
-                                <option value="Anniversary">Anniversary</option>
-                            </select>
-                        </div>
-                        <div id="submit-reserve" className="col-md-12">
-                            <input className="btn btn-outline-success w-50" type="submit" value="Make Your reservation"/>
-                        </div>
-                    </form>
+                    <Formik
+                        onSubmit={
+                            (value)=>{
+                                console.log(value)
+                                props.submitForm(
+                                    {
+                                        resDate : value["res-date"],
+                                        restime : value["res-time"],
+                                        guests : value["guests"],
+                                        occasion : value["occasion"]
+                                    }
+                                )
+                                props.updateTimes(timeId.current);
+                            }
+                        }
+                        initialValues={
+                            {
+                              "res-date" : "",
+                              "res-time" : "" ,
+                              "guests" : "1",
+                              "occasion" : "",
+                            }
+                        }
+                        validationSchema={formSchema}
+                    >
+                        <Form className="row">
+                            <div className="col-md-6">
+                                <label class="form-label" htmlFor="res-date">Choose date</label>
+                                <Field
+                                    name="res-date"
+                                    type="date"
+                                    className="form-control"
+                                />
+                                <ErrorMessage name="res-date"
+                                    render={(msg)=>
+                                    <div 
+                                    style={{color:"red",textAlign:"right"}}>
+                                    {msg}</div>
+                                    }
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <label  class="form-label" htmlFor="res-time">Choose time</label>
+                                <Field
+                                    name="res-time"
+                                    as="select"
+                                    className="form-select"
+                                    onClick={resTimeHandeler}
+                                >
+                                <option value={""} selected>-- Select --</option>
+                                {props.times.map(
+                                    (obj)=>{
+                                        return(
+                                        <option value={obj.time} id={obj.id} key={obj.id}>{obj.time}</option>
+                                        )
+                                    }
+                                )}
+                                </Field>
+                                <ErrorMessage name="res-time"
+                                    render={(msg)=>
+                                    <div 
+                                    style={{color:"red",textAlign:"right"}}>
+                                    {msg}</div>
+                                    }
+                                />
+                            </div>
+                            <div className="col-md-12">
+                                <label class="form-label" htmlFor="guests">Number of guests</label>
+                                <Field
+                                    name="guests"
+                                    type="number"
+                                    className="form-control"
+                                />
+                            </div>
+                            <div className="col-md-12">
+                                <label class="form-label" htmlFor="occasion">Occasion</label>
+                                <Field
+                                    name="occasion"
+                                    as="select"
+                                    className="form-select"
+                                >
+                                    <option value={""} selected>-- Select --</option>
+                                    <option value="Birthday">Birthday</option>
+                                    <option value="Anniversary">Anniversary</option>
+                                </Field>
+                                <ErrorMessage name="occasion"
+                                    render={(msg)=>
+                                    <div 
+                                    style={{color:"red",textAlign:"right"}}>
+                                    {msg}</div>
+                                    }
+                                />
+                            </div>
+                            <div id="submit-reserve" className="col-md-12">
+                                <input className="btn btn-outline-success w-50" type="submit" value="Make Your reservation"/>
+                            </div>
+                        </Form>
+                    </Formik>
                 </div>
                 <div id="reservation-image" className="col-md-6">
                     <img src={reservePhoto} alt="..."/>
--- src/components/Validation/BookingFormValidation.jsx
+++ src/components/Validation/BookingFormValidation.jsx
@@ -1,5 +1,7 @@
 import * as Yup from 'yup'
 
 export const formSchema = Yup.object().shape({
-    "res-date" : Yup.string().required("Selected date is required")
+    "res-date" : Yup.date().required("Selected date is required"),
+    "res-time" : Yup.string().required("Selected time is required"),
+    "occasion" : Yup.string().required("Selected ocassion type is required"),
 })
